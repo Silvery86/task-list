50736f86b53765024359b360e21143fab3f3e49d
giang <giang8692@gmail.com>
2025-02-16 15:18:04 +0700

    Fetch: Task list and Task detail route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
+use App\Models\Task;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('index');
+Route::get('/', function(){
+    return redirect()->route('tasks.index');
 });
+
+Route::get('/tasks', function ()  {
+    return view('index', [
+        'tasks' => Task::latest()->get(),
+    ]);
+})->name('tasks.index');
+
+Route::get('/tasks/{id}', function ($id) {
+
+        return view('show',[
+            'task' => Task::findOrFail($id),
+        ]);
+})->name('tasks.show');
